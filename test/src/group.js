b0d08a147934dbb96c1c1f9c873a75ba89f0d644
wusen <wusen@yidian-inc.com>
2017-02-28 18:29:07 +0800

    fix style

--- test/src/group.js
+++ test/src/group.js
@@ -1,14 +1,34 @@
 define(function (require, exports, module) {
-  var Group = {
-    template: "<div>{{name}}</div>",
-    data: function () {
-      return {
-        name: 'group-api'
-      }
-    },
-    methods: {
 
+  var groupInterface = [{
+    title: '创建',
+    url: '/group',
+    method: 'POST',
+    body: {
+      title: '创建的标题' ,
+      limit_amount: 20003,
+      limit_users: 30,
+      finishtime: Date.now(),
+      summary: '简介简介',
+      images: ['http://xxx', 'http://yyy'],
+      contact: '11111111111',
+      commodities: [{
+        name: '商品A',
+        price: 1234
+      }, {
+        name: '商品B',
+        price: 493
+      }],
+      custom_fields: ['手机', '电话', '地址', '自定义']
     }
-  };
-  module.exports = Group
+  }, {
+    title: '获取单条详情',
+    url: '/group/16',
+    method: 'GET'
+  }];
+
+  module.exports = {
+    title: 'group',
+    interfaces: groupInterface
+  }
 })
--- test/src/main.js
+++ test/src/main.js
@@ -2,36 +2,15 @@ define(function (require, exports, module) {
   var Vue = require('vue');
   var VueResource = require('vue-resource');
 
-  //var GroupApi = require('src/group')
+  var GroupApi = require('src/group');
 
   Vue.use(VueResource);
 
-  var interfaces = [{
-    title: '创建',
-    url: '/group',
-    method: 'POST',
-    body: {
-      title: '创建的标题' ,
-      limit_amount: 20003,
-      limit_users: 30,
-      finishtime: Date.now(),
-      summary: '简介简介',
-      images: ['http://xxx', 'http://yyy'],
-      contact: '11111111111',
-      commodities: [{
-        name: '商品A',
-        price: 1234
-      }, {
-        name: '商品B',
-        price: 493
-      }],
-      custom_fields: ['手机', '电话', '地址', '自定义']
-    }
-  }, {
-    title: '获取单条详情',
-    url: '/group/16',
-    method: 'GET'
-  }]
+
+  var EXPAND_ALL = true;
+
+  var interfaces = [];
+  interfaces.push(GroupApi);
 
 
   var getSuccessHandler = function (api) {
@@ -63,17 +42,26 @@ define(function (require, exports, module) {
       this.$el.style.display = 'block';
     },
     data: function () {
-      return {
-        apiDomain: 'http://laravel.app',
-        interfaces: interfaces.map(function (api) {
+      var vm = interfaces.map(function (item) {
+        item.expand = EXPAND_ALL;
+        item.interfaces = item.interfaces.map(function (api) {
+          api.expand = EXPAND_ALL;
           api.response = null;
           api.responseType = '';
           api.submiting = false;
           return api;
-        })
+        });
+        return item;
+      });
+      return {
+        apiDomain: 'http://laravel.app',
+        interfaces: vm
       }
     },
     methods: {
+      toggle: function (item) {
+        item.expand = !item.expand;
+      },
       send: function (api) {
         var sendUrl = this.apiDomain + api.url;
         api.submiting = true;
