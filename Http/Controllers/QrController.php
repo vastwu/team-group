122f520028ca36a48d603353d218baceaff2b009
vastwu <vincentwu53@hotmail.com>
2017-04-17 22:42:14 +0800

    ci code

--- Http/Controllers/QrController.php
+++ Http/Controllers/QrController.php
@@ -4,13 +4,23 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 
+use DB;
 use App\Http\Requests;
 
 class QrController extends Controller
 {
+  // 上边距
+  private $thumb_top = 300;
+  // 左右边距
+  private $thumb_offset = 20;
+  // 间距
+  private $thumb_spacing = 10;
+
+  private $bg_width = 600;
+  private $bg_height = 800;
+
   public function __construct()
   {
-
   }
   public function getImageSize($image) {
     $size = [
@@ -19,19 +29,56 @@ class QrController extends Controller
     ]; 
     return $size;
   }
-  public function getBgImage() {
+  public function getBgImage($width, $height) {
+    $bg = imagecreatetruecolor($width, $height);
     $img = imagecreatefromjpeg (dirname(__FILE__).'/../bg.jpg' );
-    return $img;
+    $size = $this->getImageSize($img);
+    imagecopyresized ( $bg, $img, 
+      0, 0, 
+      0, 0, 
+      $width, $height,
+      $size['width'], $size['height']
+    );
+    return $bg;
   }
   public function getQRImage() {
+    $codeSize = 330;
+    $code = imagecreatetruecolor($codeSize, $codeSize);
     $img = imagecreatefrompng (dirname(__FILE__).'/../qr.png' );
-    return $img;
+    $size = $this->getImageSize($img);
+    imagecopyresized ( $code, $img, 
+      0, 0, 
+      0, 0, 
+      $codeSize, $codeSize,
+      $size['width'], $size['height']
+    );
+    return $code;
+  }
+  public function drawThumb($bg, $thumbs, $bgSize) {
+    $offset = $this->thumb_offset;
+    $spacing = $this->thumb_spacing;
+    $thumbSize = ($bgSize['width'] - $offset * 2 - $spacing * 4) / 5;
+    $x = $offset;
+    $y = $this->thumb_top;
+    foreach ($thumbs as $url) {
+      $img = imagecreatefromjpeg($url);
+      $size = $this->getImageSize($img);
+      imagecopyresized ( $bg, $img, 
+        $x, $y, 
+        0, 0, 
+        $thumbSize, $thumbSize,
+        $size['width'], $size['height']
+      );
+      $x += $thumbSize  + $spacing;
+    }
   }
   public function drawTitle($image, $text, $color, $maxWidth, $offsetX, $offsetY) {
+    return
     // 分配颜色
     $textcolor = imagecolorallocate($image, $color[0], $color[1], $color[2]);
     $font = __DIR__.'/msyh.ttf';
     $fontSize = 31;
+    // 防死循环
     $len = 0;
     $n = 100;
     $limitX = $maxWidth - $offsetX;
@@ -52,9 +99,21 @@ class QrController extends Controller
   {
     $width = 100;
     $height = 300;
-    $title = '前端 Code-review 开团xxx';
 
-    $bg = $this->getBgImage();
+    $gid = $request->get('gid');
+    $result = DB::table('group')
+      ->where('id', $gid)
+      ->get();
+
+    $group = $result[0];
+    //var_dump($group);
+    //return $this->json($group);
+    $title = $group->title;
+    $summary = urldecode($group->summary);
+    $images = json_decode($group->images);
+    $contact = $group->contact;
+
+    $bg = $this->getBgImage($this->bg_width, $this->bg_height);
     $codeImage = $this->getQRImage();
 
     $bgImageSize = $this->getImageSize($bg);
@@ -65,6 +124,7 @@ class QrController extends Controller
     //$textcolor = imagecolorallocate($bg, 0, 0, 0);
     //imagestring($bg, 20, 15, 10, "Hello world!", $textcolor);
     $this->drawTitle($bg, $title, [0, 0, 0], $bgImageSize['width'], 20, 7);
+    $this->drawThumb($bg, $images, $bgImageSize);
     // 计算中心点
     // 默认画在y轴下 60% 的位置
     $drawYPercent = 0.6;
